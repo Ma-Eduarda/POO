d817d14247293ca58ba5a85c0ea6a4dcea1ea17c
Maria Eduarda <eduardabritto2014@gmail.com>
2024-11-28 17:50:16 -0300

    Update calendario.java

--- calendario.java
+++ calendario.java
@@ -32,7 +32,7 @@ public class Calendario {
     }
 
     public void anoBissexto() {
-        if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+        if (ano % 4 == 0) {
             System.out.println(ano + " é um ano bissexto.");
         } else {
             System.out.println(ano + " não é um ano bissexto.");
